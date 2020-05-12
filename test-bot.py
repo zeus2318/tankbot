ff96deedc713b914ad0568ec95467dafe87d8b27
wawa013 <49781867+wawa013@users.noreply.github.com>
2020-05-12 16:59:01 +0800

    Update test-bot.py

--- test-bot.py
+++ test-bot.py
@@ -16,7 +16,7 @@ players = {}
 
 @client.event
 async def on_ready():
-    await client.change_presence(status=discord.Status.dnd, activity=discord.Activity(type=discord.ActivityType.watching, name="Movie"))
+    await client.change_presence(status=discord.Status.dnd, activity=discord.Activity(type=discord.ActivityType.watching, name="t!help"))
     print(f'\nLogged in as {client.user.name}#{client.user.discriminator}, User ID: {client.user.id}, Version: {discord.__version__}\n')
 
 @client.event
